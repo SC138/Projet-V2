9a22b090095fd58ac1090358744269b42f67d701
Thomas Chevron <chevron.thomas33@gmail.com>
2022-08-30 15:12:38 +0200

    CRUD admin

--- src/Controller/AdminAdminController.php
+++ src/Controller/AdminAdminController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Client\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -48,4 +49,49 @@ class AdminAdminController extends AbstractController
 
     }
 
+    /**
+     * @Route ("/admin/update", name="admin_update")
+     */
+
+    public function updateAdmin(UserRepository $userRepository, Request $request, EntityManagerInterface $entityManager, User $user, UserPasswordHasherInterface $userPasswordHasher){
+
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $userRepository->add($user, true);
+            $plainPassword = $form->get('password')->getData();
+            $hashedPassword = $userPasswordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Mot de passe modifié');
+
+            return $this ->redirectToRoute("home-page");
+        }
+
+        return $this->renderForm('admin/update_admin.html.twig', [
+            'user'=> $user,
+            'form'=> $form,
+        ]);
+    }
+
+    /**
+     * @Route ("admin/delete/admin/{id}", name="admin_delete_admin"
+     */
+
+    public function deleteAdmin($id, UserRepository $userRepository, EntityManagerInterface $entityManager){
+        $user = $userRepository->find($id);
+        if (!is_null($user)){
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Le compte est bien supprimé');
+            return $this->redirectToRoute("home-page");
+        } else {
+            return new Response('Compte introuvable');
+        }
+    }
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -14,16 +14,10 @@ class LoginController extends AbstractController
      */
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
 
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
