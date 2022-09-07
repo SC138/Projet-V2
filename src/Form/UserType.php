f4a064e592c97b3820014164242b2709b1d2f875
Thomas Chevron <chevron.thomas33@gmail.com>
2022-09-07 17:13:23 +0200

    Continue CSS

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -15,16 +16,18 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class, ['attr'=>['class'=>'form-control']])
             ->add('password', RepeatedType::class, [
+                'options'=>['attr'=>['class'=>'form-control']],
                 'type' => PasswordType::class,
                 'invalid_message' => 'Mot de passe invalide',
-                'first_options' => ['label' => 'Mot de passe'],
+                'first_options' => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Confirmer mot de passe'],
                 //POUR LE CSS: pour ajouter une classe sur un champ FORM: 'attr' => ['class' => 'nom de ma classe']
 //                'second_option' => ['label' => 'Confirmer le mot de passe'],
 
             ])
-            ->add('Modifier',SubmitType::class)
+            ->add('Modifier',SubmitType::class, ['attr'=>['class'=>'form-control my-4']])
         ;
     }
 
